9c5923db02689e790c2c47a05bb186bc3c058068
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-06-08 17:20:59 +0600

    update the card and context processor

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -62,6 +62,8 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+            #request for cart
+                'cart.context_processors.cart',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
